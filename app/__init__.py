c4ffc0edd097f3efc1a4dbf08cccc8c90ad6d8b0
Jonathan Kerr <jonathandrewkerr@gmail.com>
2018-12-09 09:26:30 +0000

    Most of the user journey and hashing

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 
 def create_app():
     application = Flask(__name__, static_url_path='/static')
+    application.config.update(SECRET_KEY='SECRET-KEY')
     from app.main import bp as main_bp
     application.register_blueprint(main_bp)
 
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,23 +1,39 @@
 from app.main import bp
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash, session
+import hashlib
 
 
 @bp.route('/')
-def hello_world():
+def index():
     return render_template('base.html')
 
 
 @bp.route('/video', methods=['GET', 'POST'])
 def video():
-    print(request.method)
     if request.method == 'POST':
+        print(request.files['file-upload-1'])
         # TODO: spin up bucket and dump video, details of user
-        pass
+        if 'file-upload-1' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        print(request.files)
+        session['hash'] = checksum_md5(request.files['file-upload-1'])
+        return redirect(url_for('main.complete'))
     return render_template('main/video.html', heading="Submit video evidence")
 
 
 @bp.route('/complete')
 def complete():
-    return render_template('main/complete.html', hash='g3ny9d'.upper())
+    return render_template(
+        'main/complete.html',
+        hash=session['hash'][0:6] or 'TEST HASH',
+        heading='Submission complete'
+    )
 
 
+def checksum_md5(filename):
+    md5 = hashlib.md5()
+    with filename.stream as f:
+        for chunk in iter(lambda: f.read(8192), b''):
+            md5.update(chunk)
+    return md5.hexdigest()
