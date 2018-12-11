affc86866b8fbb13f09b8e698c216ef127cc0699
Jonathan Kerr <jonathandrewkerr@gmail.com>
2018-12-11 18:04:45 +0000

    State as of the end of the weekend

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import os
 
 
 def create_app():
     application = Flask(__name__, static_url_path='/static')
+
+    APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+    UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static', 'customlogos')
+    application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     application.config.update(SECRET_KEY='SECRET-KEY')
+
     from app.main import bp as main_bp
     application.register_blueprint(main_bp)
 
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,9 @@
 from app.main import bp
 from flask import render_template, request, redirect, url_for, flash, session
+from werkzeug.utils import secure_filename
 import hashlib
+import boto3
+import os
 
 
 @bp.route('/')
@@ -8,16 +11,41 @@ def index():
     return render_template('base.html')
 
 
+@bp.route('/details', methods=['GET', 'POST'])
+def details():
+    if request.method == 'POST':
+        session['details'] = request.form
+        print(session['details'])
+        return redirect(url_for('main.video'))
+    return render_template('main/details.html')
+
+
+@bp.route('/declaration')
+def declaration():
+    return render_template('main/declaration.html', heading='Declaration')
+
+
 @bp.route('/video', methods=['GET', 'POST'])
 def video():
     if request.method == 'POST':
-        print(request.files['file-upload-1'])
         # TODO: spin up bucket and dump video, details of user
         if 'file-upload-1' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        print(request.files)
+        # TODO: limit the kinds of files that can be uploaded
+        print(request.form)
         session['hash'] = checksum_md5(request.files['file-upload-1'])
+        file = request.files['file-upload-1']
+        filename = secure_filename(file.filename)
+        s3 = s3_client()
+        # s3.create_bucket(
+        #     Bucket='test-htp',
+        #     CreateBucketConfiguration={
+        #         'LocationConstraint': 'eu-west-1'
+        #     },
+        # )
+        s3 = boto3.resource('s3')
+        # s3.Bucket('test-htp').upload_file
         return redirect(url_for('main.complete'))
     return render_template('main/video.html', heading="Submit video evidence")
 
@@ -37,3 +65,9 @@ def checksum_md5(filename):
         for chunk in iter(lambda: f.read(8192), b''):
             md5.update(chunk)
     return md5.hexdigest()
+
+
+def s3_client() -> boto3.client:
+    return boto3.client('s3', region_name='eu-west-1')
+
+
